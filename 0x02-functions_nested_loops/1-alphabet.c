b61362ad9865062aec7d3354d1ee0ac9b3323c34
Rabbi Ofori Gyimah <rabbioforigyimah@gmail.com>
2023-02-22 15:34:04 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,14 @@
  * print_alphabet - print alphabet in lowercases followed by a new line
  */
 
-void print_alphabet(void);
-void print_alphabet()
-{                
-	        char c;
-		for (c = "a"; c <= "z"; c++)
-			{
-				_putchar(c);
-			}
-		_putchar("\n");
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}	_putchar('\n');
 }
