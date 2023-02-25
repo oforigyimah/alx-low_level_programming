7772221859785e990fa1d66f607dc7a70ca2ec47
Ofori Gyimah <rabbioforigyimah@gmail.com>
2023-02-24 23:28:52 -0800

    code fixed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,12 @@
 void print_most_numbers(void)
 {
 		int n;
-		for (n = '0'; n < '10'; n++)
+
+		for (n = 0; n < 10; n++)
 			{
-				if (n == '2' || n == '4')
-						continue;
-				_putchar(n);
+			if (n == 2 || n == 4)
+				continue;
+		  _putchar(n + '0');
 			}
 		_putchar('\n');
 }
