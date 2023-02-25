91e89f10288a0db1b8fd1705a4b865bcc937aacd
Ofori Gyimah <rabbioforigyimah@gmail.com>
2023-02-24 23:56:40 -0800

    style fixed

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,11 +10,12 @@
 void print_line(int n)
 {
 		int i;
+
 		for (i = 0; i < n; i++)
 			{
-				if (n <= 0)
-						break;
-				_putchar('_');
+			if (n <= 0)
+				break;
+			_putchar('_');
 			}
 		_putchar('\n');
 }
