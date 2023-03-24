7bc2353c9cd8cf3305b3296e579ad67c7d56656c
Ofori Gyimah <rabbioforigyimah@gmail.com>
2023-03-23 22:54:52 -0700

    update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - sum of all the parameters 
+ * sum_them_all - sum of all the parameters
  * @n: count of var
  *
  * Return: sum
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,10 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			return;
 		for (i = 0; i < n; i++)
 			{
-				if (i == n - 1)
-					printf("%d", va_arg(args, int));
-				else
-					printf("%d%s", va_arg(args, int), *separator);
+			if (i == n - 1)
+				printf("%d", va_arg(args, int));
+			else
+				printf("%d%s", va_arg(args, int), *separator);
 			}
 		printf("\n");
 		va_end(args);
